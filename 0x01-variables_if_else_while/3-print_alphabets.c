cd7534cfefc1da04a1c813d4c3ac8b19024353b9
Segun <ojoolusegun17@gmail.com>
2022-09-09 09:56:35 -0700

    alpHABETs

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
