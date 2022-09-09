75dcd9309e87bc1ab520ef84e97c85df4cb4c30d
Segun <ojoolusegun17@gmail.com>
2022-09-09 15:26:23 -0700

    invention is a combo of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description: write a program that prints all possible combinations of two digits.
+ * Numbers must be separated by comma followed by a space.
+ * The two digits must be different. *
+ * 01 and 10 are considered the same combination of the two digits 0 and 1.
+ * Print only the smallest combination of two digits.
+ * Numbers should be printed in ascending order, with two digits.
+ * You can only use the putchar function, not more than 5 times.
+ * Do not use any variable of type `char`.
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	i = 0;
+
+	while (i <100)
+	{
+		j = i % 10; /*single digits */
+		k = i / 10; /*double digits */
+
+		if (k < j)
+		{
+			putchar(k + '0');
+			putchar(j + '0');
+
+			if (i < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
