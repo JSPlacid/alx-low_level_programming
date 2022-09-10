a15c406fb170c682402e1c905e9a3a80198aed5a
Segun <ojoolusegun17@gmail.com>
2022-09-09 22:50:02 -0700

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry point
- * Description: Prints all possible combos of two digit numbers.
+ * Description: Prints all possible combos of two 2-digit numbers.
  * The two numbers should be separated by a space.
  * Return: Always 0
  */
@@ -19,7 +19,7 @@ int main(void)
 		a = i / 10; /*doubles fnum*/
 		b = i % 10; /*singles fnum*/
 
-		for (j = 0; j < 100; j++)
+		for (j = i + 1; j < 100; j++)
 		{
 			c = j / 10; /*doubles snum*/
 			d = j % 10; /*singles snum*/
