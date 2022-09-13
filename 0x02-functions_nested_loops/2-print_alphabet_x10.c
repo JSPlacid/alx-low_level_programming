6b76f9f40b089151f5c6ba5be6e9d870b2ffd89b
Segun <ojoolusegun17@gmail.com>
2022-09-13 07:42:49 -0700

    looping alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints 10 times the alphabets in lowercase, followed by a new line.
+ * Return: Nothing.
+ */
+
+void print_alpha_x10(void)
+{
+	int i;
+	int alp;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for(alp = 'a'; alp <= 'z' alp++)
+		{
+			putchar(alp);
+		}
+		putchar('\n');
+	}
+}
