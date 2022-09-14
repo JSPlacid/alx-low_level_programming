be4b036977c84ac783c978ba85fb4089aca7868d
Segun <ojoolusegun17@gmail.com>
2022-09-14 15:01:14 -0700

    A function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,49 @@
 #include "main.h"
 /**
  * print_times_tables - print the time table for n.
- * Description: print multiplication table
+ * Description: if `n` is greater than 15 or less than 0, return null.
  * @n: The multiplication table requested.
  * Return: Nothing.
  */
 void print_times_table(int n)
 {
-	int i, j, rslt;
+	int i = 0, y, z;
 
-	if (!(n > 15 || n < 0))
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		for (i = 0; i <= n; i++)
+		for (y = 0; y <= n; y++)
 		{
-			for (j = 0; j < n; j++)
+			z = x * y;
+			if (z > 99)
 			{
-				rslt = (i * j);
-				if (j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				if (rslt < 10 && j != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((rslt % 10) + '0');
-				}
-				else if (rslt >= 10 && rslt < 100)
-				{
-					_putchar(' ');
-					_putchar((rslt / 10) + '0');
-					_putchar((rslt % 10) + '0');
-				}
-				else if (rslt >= 100 && j != 0)
-				{
-					_putchar((rslt / 100) + '0');
-					_putchar((rslt / 10) % 10 + '0');
-					_putchar((rslt / 10) + '0');
-				}
-				else
-					_putchar((rslt % 10) + '0');
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
 			}
-			_putchar('\n');
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0);
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
+		x++;
 	}
 }
