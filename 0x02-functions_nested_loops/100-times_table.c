5395b61e9eb1b7cb189e90c4820e77db121b4925
Segun <ojoolusegun17@gmail.com>
2022-09-14 15:24:25 -0700

    A function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_times_tables - print the `n` times table from 0
+ * print_times_table - print the `n` times table from 0
  * Description: if `n` is greater than 15 or less than 0, return nothing.
  * @n: The multiplication table requested.
  */
