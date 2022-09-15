7c15060f95957013327898558b4c38d4a30d26bd
Segun <ojoolusegun17@gmail.com>
2022-09-15 14:12:52 -0700

    A function that prints numbers 0 to 14, 10 places each on a new line.

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers from 0 to 14 ten places each on new line
+ * Description: use _putchar only 3 times
+ */
+
+void more_numbers(void)
+{
+	int c, i;
+
+	c = 0;
+	while (c < 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			i++;
+		}
+		_putchar('\n');
+	}
+}
