05b0dd2c6f22aea458ef60be2c3d7deb4616b9f8
Segun <ojoolusegun17@gmail.com>
2022-09-15 15:06:48 -0700

    Write a function that draws a diagonal line on the terminal.

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * mul - multiplies two operands
- * @a: first operand
- * @b: second operand
- * return 0
+ * mul - multiplies two integers
+ * @a: first operand.
+ * @b: second operand.
+ * return: a * b result
  */
 
 int mul(int a, int b)
 {
 	return (a * b);
-	return (0);
 }
