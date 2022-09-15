f92575a6b486120757f074fe3c7072b84eb77d86
Segun <ojoolusegun17@gmail.com>
2022-09-15 16:42:22 -0700

    Write a program that prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz.

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints the numbers from 1 to 100
+ *  for multiples of three print Fizz
+ *  instead of the number and for the multiples of five print Buzz
+ *  numbers which are multiples of both three and five print FizzBuzz.
+ *  Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			print("Fizz");
+		else if (i % 5 == 0)
+			print("Buzz");
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
