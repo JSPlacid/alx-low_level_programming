50920dd2e396018f1e88557564d11d0cbc5c7f97
Segun <ojoolusegun17@gmail.com>
2022-09-16 03:36:04 -0700

    Write a function that prints a square, followed by a new line.

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,2 +1,14 @@
 #include "main.h"
 
+/**
+ * print_triangle -  prints a triangle, followed by a new line
+ * Description: You can only use _putchar function to print
+ * Where size is the size of the triangle
+ * If size is 0 or less, the function should print only a new line
+ * Use the character # to print the triangle
+ * @size: size of triangle
+ */
+
+void print_triangle(int size)
+{
+
