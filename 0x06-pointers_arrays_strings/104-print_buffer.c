3df105be967e87c0c061e682a0bb606ea8f424d8
Segun <ojoolusegun17@gmail.com>
2022-09-22 13:31:43 -0700

    Write a function that prints a buffer.

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints a buffer.
+ * @b: pointer to string
+ * @size: total number of bytes
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+	int c, g;
+
+	c = 0;
+	if (<= 0)
+	{
+		printf("\n");
+	}
+	while (c < size)
+	{
+		printf("%8.8x:", c);
+		d = 0;
+		while (g < 10)
+		{
+			printf("%02x", b[c + g]);
+			if ((g % 2 == 0 && g != 0) || (c + g > size - 1))
+			{
+				printf(" ");
+			}
+			g++;
+		}
+		c += 10;
+		printf("\n");
+	}
+}
