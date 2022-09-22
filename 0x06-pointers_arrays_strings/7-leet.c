824d8be156f8fb7cd6ef0f2051c13cbde87eb0ff
Segun <ojoolusegun17@gmail.com>
2022-09-22 08:12:00 -0700

    Write a function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,16 +8,16 @@
 
 char *leet(char *s)
 {
-	int i, j;
-	char subs[] = "aAeEoOtTlL";
-	char le = "43071";
+	int i = 0, j;
+	char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
+	    str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
-	i = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i])
 	{
-		for (j = 0; subs[j] != '\0'; j++)
-			if (s[i] == subs[j])
-				s[i] = le[j / 2];
+		for (j = 0; j < 10; j++)
+			if (s[i] == str[j])
+				s[i] = subs[j];
+		i++;
 	}
 	return (s);
 }
