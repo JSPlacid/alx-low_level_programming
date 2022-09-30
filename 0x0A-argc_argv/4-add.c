bf761e8ca1cbd39bc6ade92e142b8d6a695dd6b4
Segun <ojoolusegun17@gmail.com>
2022-09-30 11:27:19 -0700

    Write a program that adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - add 2 operands and prints result
+ * @argc: argument count
+ * @argv: argument vector
+ * Description: if no number is passed, print 0
+ * if one operand is NaN, print Error
+ * Return: 1 if error, else 0
+ */
+int main(int argc, char *argv[])
+{
+	int total, i;
+	char p;
+	int num;
+
+	total = 0;
+	if (argc > 1)
+	{
+		for (i = 1; argv[i]; i++)
+		{
+			num = strtol(argv[i], &p, 10);
+			if (!*p)
+				total += num;
+			else {
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", total);
+	return (0);
+}
