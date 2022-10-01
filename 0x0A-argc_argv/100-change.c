2d93c69c6d602e617f03252172127835b59c3c4d
Segun <ojoolusegun17@gmail.com>
2022-09-30 21:15:20 -0700

    coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
 /**
  * main -  prints the minimum number of coins
@@ -14,49 +12,33 @@
 
 int main(int argc, char *argv[])
 {
-	unsigned int cents;
-	int coins;
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	cents = coins = 0;
-	if (argc == 2)
+
+	if (argc != 2)
 	{
-		if (argv[1][0] == '-')
-			printf("0\n");
-		if (checker(argc, 1, 0, argv) == 0)
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
 		{
-			cents = atoi(argv[1]);
-			for (; cents >= 25; coins++, cents -= 25)
-				;
-			for (; cents >= 10; coins++, cents -= 10)
-				;
-			for (; cents >= 5; coins++, cents -= 5)
-				;
-			for (; cents >= 2; coins++, cents -= 2)
-				;
-			for (; cents >= 1; coins++, cents--)
-				;
-			printf("%d\n", coins);
+			result++;
+			num -= coins[j];
 		}
 	}
-	else
-		printf("Error\n");
-	return (0);
-}
-
-/**
- * checker - checks for valid input
- * @argc: argument count
- * @argv: argument vector
- * @i: counter for argv[]
- * @j: counter for argv[][]
- * Return: 0, else 1
- */
-
-int checker(int argc, int i, unsigned int j, char *argv[])
-{
-	for (i = 1; i < argc; i++)
-		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]; j++)
-				if (isdigit(argv[i][j]) == 0)
-				return (1);
+	printf("%d\n", result);
 	return (0);
 }
