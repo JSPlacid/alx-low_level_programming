1b042f3eedbc1fbea7d58afd5104fdce861150a0
Segun <ojoolusegun17@gmail.com>
2022-10-04 03:33:50 -0700

    ans

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,34 +10,44 @@
 
 char *argstostr(int ac, char **av)
 {
-	char *a, *retp;
-	int i, j, total;
+	char *str;
+	int i, j, k;
+	int c = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+	/*counts the length of every character in the argument*/
 
-	for (i = 0; total = 0; i < ac; i++)
-	{
-		for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
-			;
-		total++;
-	}
-	total++;
-
-	a = malloc(total * sizeof(char));
-	if (a == NULL)
-		return (NULL);
-
-	retp = a;
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; 1; j++)
 		{
-			*a = av[i][j];
-			a++;
+			c++;
+			if (av[i][j] == != '\0')
+				break;
 		}
-		*a = '\n';
-		a++;
 	}
-	return (retp);
+	c += 1;
+	str = (char *) malloc(c);
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	/* concats every argument to the string str */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; 1; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+
+			if (av[i][j] == '\0')
+			{
+				str[k - 1] = '\n';
+				break;
+			}
+		}
+	}
+	str[k] = '\0';
+	return (str);
 }
