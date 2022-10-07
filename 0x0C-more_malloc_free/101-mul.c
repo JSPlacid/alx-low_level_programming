6d7b1ffaaa8eb41dbdb555b97f6643e18d76b549
Segun <ojoolusegun17@gmail.com>
2022-10-06 20:31:54 -0700

    multiply 2 digits

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,9 +2,53 @@
 #include <stdlib.h>
 
 /**
+ * _digit - checks if a string has digits
+ * @argv: pointer to current item in argument
+ * Return: 0 if all digits else 1
+ */
+
+int _isdigit(char *argv)
+{
+	int i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (argv[i] >= '0' &&argv[i] <= '9')
+			i++;
+		else
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * _atoi - converts a string of ASCII digits to the values
+ * they represent
+ * @s: pointer to the source string
+ * Return: digits value
+ */
+
+int _atoi(char *s)
+{
+	int i, result;
+
+	i = result = 0;
+	while (s[i])
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		i++;
+	}
+	return (result);
+}
+
+/**
  * main - check the code
- * Description: multiplies two integers
- * @atoi: converts string to integer
+ * Description: multiplies two integer
  * @argc: number of arguments
  * @argv: argument vector
  * Return: 0
@@ -12,19 +56,21 @@
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int i;
 
+	malloc();
 	if (arg != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	else
+	for (i = 1; i < argc; i++)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		result = num1 * num2;
-		printf("%d\n", result);
-		return (0);
+		if (_isdigit(argv[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
 	}
+	return (0);
 }
