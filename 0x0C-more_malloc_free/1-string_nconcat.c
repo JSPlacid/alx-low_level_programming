36ea3affd28f4615c61090771f4e6e2a966cb31c
Segun <ojoolusegun17@gmail.com>
2022-10-06 21:49:05 -0700

    Write a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,41 +1,50 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * string_nconcat - concats two strings, only n bytes of s2
+ * string_nconcat - concats 2 strings, only n bytes of s2
  * @s1: string 1
  * @s2: string 2
- * @n: input integer of number of string to concatenate
- * Return: A pointer to concatenate string or NULL its string is NULL
+ * @n: bytes to include of s2
+ * Return: NULL if fails, else pointer to malloc memory
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *new_str;
-	unsigned int i = 0, lens1 = 0, lens2 = 0;
-	
+	char *p;
+	int strlen1, i, c;
+
 	if (s1 == NULL)
 		s1 = "";
-	while (s1[lens1])
-		lens1++;
-
 	if (s2 == NULL)
 		s2 = "";
-	while (s2[lens2])
-		lens2++;
 
-	if (n >= lens2)
-		n = lens2;
-	new_str = malloc(lens1 + n + 1);
-	if (new_str == NULL)
+	strlen1 = (unsigned int)_strlen(s1);
+	p = malloc((strlen1 + n + 1) * sizeof(char));
+	if (p == NULL)
 		return (NULL);
-	for (; i < (lens1 + n); i++)
+	for (i = 0, c = 0; i < (strlen1 + n); i++)
 	{
-		if (i < lens1)
-			new_str[i] = *s1, s1++;
+		if (i < strlen1)
+			p[i] = s1[i];
 		else
-			new_str[i] = *s2, s2++;
+			p[i] = s2[c++];
 	}
-	new_str[i] = '\0';
-	return (new_str);
+	p[i] = '\0';
+	return (p);
+}
+/**
+ * _strlen - find length of a string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
 }
