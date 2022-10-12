c536a17cd5f7d20a31cf3e5bb4c77382e650b5b3
Segun <ojoolusegun17@gmail.com>
2022-10-11 18:00:26 -0700

    create a new dog struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,24 +21,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 		;
 	doggy = malloc(sizeof(dog_t));
 	if (doggy == NULL)
+	{
 		free(doggy);
-	return (NULL);
+		return (NULL);
+	}
 	doggy->name = malloc(nlen * sizeof(doggy->name));
 	if (doggy->name == NULL)
+	{
 		free(doggy->name);
-	free(doggy);
-	return (NULL);
+		free(doggy);
+		return (NULL);
+	}
 	for (i = 0; i < nlen; i++)
+	{
 		doggy->name[i] = name[i];
+	}
 	doggy->age = age;
-
 	doggy->owner = malloc(olen * sizeof(doggy->owner));
 	if (doggy->owner == NULL)
+	{
 		free(doggy->owner);
-	free(doggy->name);
-	free(doggy);
-	return (NULL);
+		free(doggy->name);
+		free(doggy);
+		return (NULL);
+	}
 	for (i = 0; i < olen; i++)
-		doggy->owner[i] = owner[i];	
+		doggy->owner[i] = owner[i];
 	return (doggy);
 }
