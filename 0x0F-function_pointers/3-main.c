cb23edd7b723e8927c8d0ade0d8e4114a9cced5b
Segun <ojoolusegun17@gmail.com>
2022-10-18 08:46:43 -0700

    corr

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,27 +10,34 @@
  * Return: 0, 98 0n argc err, 99 on illegal operator
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int (*func)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (arg[2][1] != '\0')
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	func = get_op_func(argv[2]);
-	if (func == NULL)
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
-	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
