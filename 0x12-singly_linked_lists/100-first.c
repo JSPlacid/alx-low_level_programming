812938933182b55668491814a795a11d3bbd3290
Segun <ojoolusegun17@gmail.com>
2022-10-20 16:26:17 -0700

    print some text before the main function is exec

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
+void __attribute__ ((constructor)) hare(void);
+
 /**
-  * b - prints from init by the loader before main
-  */
+ * hare - prints a string before the main func is exec
+ */
 
-void b(void) __attribute__ ((constructor));
-
-void b(void)
+void hare(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
